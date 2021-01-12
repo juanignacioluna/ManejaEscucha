33da3499bce822a126cea37b4c4afbab46392956
Juan Ignacio Luna <chinoluna11@gmail.com>
2021-01-12 04:38:43 -0300

    Updates

---
+++ precache-manifest.5da39a5d9aabb4a0553e557919f40f84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5613ae4b813d14f55576e08f43600cc5",
+    "url": "/ManejaEscucha/index.html"
+  },
+  {
+    "revision": "d50a5b3487dc9675b50e",
+    "url": "/ManejaEscucha/static/css/main.26519230.chunk.css"
+  },
+  {
+    "revision": "99684668b64929a80d3b",
+    "url": "/ManejaEscucha/static/js/2.dd20ed1c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ManejaEscucha/static/js/2.dd20ed1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d50a5b3487dc9675b50e",
+    "url": "/ManejaEscucha/static/js/main.ecd677a2.chunk.js"
+  },
+  {
+    "revision": "05dddf64509fdb3caec4",
+    "url": "/ManejaEscucha/static/js/runtime-main.b3711bc0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ManejaEscucha/static/media/logo.5d5d9eef.svg"
+  }
+]);
